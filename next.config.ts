2905095dfdc1b18ef06391d347eeb77569dc2e3f
Kostya <kostyantyn.karimov@gmail.com>
2025-02-12 19:12:28 +0200

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? "/vehicle-finder" : "",
-  assetPrefix: isProd ? "/vehicle-finder/" : "",
   typescript: {
     ignoreBuildErrors: true,
   },
